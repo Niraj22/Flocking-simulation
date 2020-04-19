d2d7a7e64ee92d10c4c783809af5b7e0b0b6e986
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2020-04-19 20:18:49 +0545

    Added slider for control of variables

--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ let alignSlider, cohesionSlider, seperationSlider
 function setup(){
     textSize(8);
     createCanvas(windowWidth, windowHeight*(0.80))
+    createP('Allingment')
     alignSlider = createSlider(0, 5, 1, 0.1)
-    createP('')
+    createP('Cohesion')
     cohesionSlider = createSlider(0, 5, 1, 0.1)
-    createP('')
+    createP('Seperation')
     seperationSlider = createSlider(0, 5, 1, 0.1)
     for (let index = 0; index < 150; index++) {
         flock.push(new Boid()) 
@@ -14,7 +15,7 @@ function setup(){
     
 }
 function draw(){
-    background(29);
+    background(29)
     for(let boid of flock){
         boid. edges()
         boid.flock(flock)
